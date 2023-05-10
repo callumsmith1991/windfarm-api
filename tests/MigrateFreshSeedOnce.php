6c9d0ac8d1a8d2f5e0eb2c9f105940f59c07f3e8
Callum Smith <callumsmith1804@outlook.com>
2023-05-10 21:37:02 +0100

    migrate fresh on testing database

--- tests/MigrateFreshSeedOnce.php
+++ tests/MigrateFreshSeedOnce.php
@@ -3,29 +3,35 @@
 /* https://medium.com/helpspace/fresh-database-once-befor-testing-starts-faa2b10dc76f */
 
 namespace Tests;
+
 use Illuminate\Support\Facades\Artisan;
+
 trait MigrateFreshSeedOnce
 {
     /**
-    * If true, setup has run at least once.
-    * @var boolean
-    */
+     * If true, setup has run at least once.
+     * @var boolean
+     */
     protected static $setUpHasRunOnce = false;
     /**
-    * After the first run of setUp "migrate:fresh --seed"
-    * @return void
-    */
-    public function setUp() : void
+     * After the first run of setUp "migrate:fresh --seed"
+     * @return void
+     */
+    public function setUp(): void
     {
         parent::setUp();
         if (!static::$setUpHasRunOnce) {
             Artisan::call('migrate:fresh');
             Artisan::call(
-                'db:seed', ['--class' => 'DatabaseSeeder']
+                'db:seed',
+                ['--class' => 'DatabaseSeeder']
             );
             static::$setUpHasRunOnce = true;
-         }
+        }
     }
-}
 
-?>
+    // public function tearDown(): void 
+    // {
+    //     Artisan::call('migrate:rollback');
+    // }
+}
